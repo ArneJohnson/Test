5179bb228a4ce8cc04ffef6a63922482395b0804
Arne Johnsson <20273@NYNASHAMN.SE>
2021-03-25 11:35:35 +0100

    first commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/index")
+	public String index() {
+		
+		return "index";
+	}
+	@RequestMapping("/main")
+	public String main() {
+		
+		return "index";
+	}
+	@RequestMapping("/menu")
+	public String menu() {
+		
+		return "index";
+	}
+}
